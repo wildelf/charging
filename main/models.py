0dffd5a846419ba92caf7502822e9a6c73ad5843
wilde <wilde@qq.com>
2018-12-03 01:44:20 +0800

    add models

--- main/models.py
+++ main/models.py
@@ -2,6 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class User(models.Model):
+    name = models.CharField(max_length=64,verbose_name='用户名')
+    session_key = models.CharField(max_length=64)
+    openid = models.CharField(max_length=64)
+    token = models.CharField(max_length=64)
+    pwd = models.CharField(max_length=64)
+    pay_list = models.ForeignKey('PayList',verbose_name='支付列表')
+
+class PayList(models.Model):
+    datetime = models.IntegerField()
+
+
+
 class Password(models.Model):
     device_id = models.CharField(max_length=64,verbose_name='设备id')
     password = models.CharField(max_length=4,verbose_name='密码')
